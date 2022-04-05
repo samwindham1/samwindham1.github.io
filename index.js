1d990f3541bbe27d8a3957c44f371dee8845d74a
samwindham1 <sam.windham95@gmail.com>
2022-04-05 11:48:27 -0500

    Add projects

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var elementIsVisible = (elem) => {
     let elemTop = rect.top;
     let elemBottom = rect.bottom;
 
-    return elemTop >= 0 && elemBottom <= window.innerHeight;
+    return elemTop >= 0 || elemBottom >= window.innerHeight;
 };
 
 var setVisibleSection = (section) => {
-    if (visibleSection != section) {
+    if (!!section && visibleSection != section) {
         getMenuElement(visibleSection?.id)?.classList.remove("active");
         getMenuElement(section?.id)?.classList.add("active");
         visibleSection = section;
@@ -52,15 +52,54 @@ if (!/Mobi|Android/i.test(navigator.userAgent)) {
 
 var introH1 = document.getElementById("intro");
 
-var split = introH1.innerText.split(" ");
+var split = introH1.getElementsByClassName("intro-inner")[0].innerText.split(" ");
 var str = split
     .map(
         (word, i) =>
-            `<span style="overflow: hidden"><span id="intro-span-${i}" style="animation-delay: ${
-                i * (1 / (split.length + 1))
-            }s;">${word}</span></span>`
+            `<span id="intro-span-${i}" style="animation-delay: ${i * (1 / (split.length + 1))}s;">${word}</span>`
     )
     .join(" ");
-console.log(str);
 var spans = document.createRange().createContextualFragment(str);
 introH1.replaceChildren(spans);
+
+//
+// Add tabs to gist examples
+//
+
+var createElementFromHTML = (htmlString) => {
+    var div = document.createElement("div");
+    div.innerHTML = htmlString.trim();
+
+    return div.firstChild;
+};
+
+var hideAllElements = (elements) => {
+    elements.forEach((element) => {
+        element.style.display = "none";
+    });
+};
+
+var gist_examples = Array.from(document.getElementsByClassName("gist"));
+gist_examples.forEach((gist) => {
+    let files = Array.from(gist.getElementsByClassName("gist-file"));
+    if (files.length > 1) {
+        let header = createElementFromHTML(`<div class='gist-header'></div>`);
+        hideAllElements(files);
+        files[0].style.display = "block";
+
+        files.forEach((file, index) => {
+            let filename = file.getElementsByTagName("a")[1].innerText.trim();
+            let tab = createElementFromHTML(
+                `<div class='gist-tab${index == 0 ? " active" : ""}'>` + filename + "</div>"
+            );
+            tab.addEventListener("click", (e) => {
+                hideAllElements(files);
+                file.style.display = "block";
+                Array.from(header.getElementsByClassName("active")).forEach((tab) => tab.classList.remove("active"));
+                tab.classList.add("active");
+            });
+            header.appendChild(tab);
+        });
+        gist.prepend(header);
+    }
+});
